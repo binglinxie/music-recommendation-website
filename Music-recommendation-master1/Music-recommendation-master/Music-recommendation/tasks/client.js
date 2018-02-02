b352d8fac093bd09979b35e047c8f2316075743a
binglinxie <bxie3@stevens.edu>
2018-02-02 00:59:43 -0500

    getaccesstokenUsingclientId

--- Music-recommendation-master1/Music-recommendation-master/Music-recommendation/tasks/client.js
+++ Music-recommendation-master1/Music-recommendation-master/Music-recommendation/tasks/client.js
@@ -1,4 +1,4 @@
-var SpotifyWebApi = require("../");
+var SpotifyWebApi = require('spotify-web-api-node');
 
 /**
  * This example retrives an access token using the Client Credentials Flow. It's well documented here:
@@ -14,19 +14,23 @@ var SpotifyWebApi = require("../");
  * https://developer.spotify.com/my-applications
  */
 var spotifyApi = new SpotifyWebApi({
-  clientId : '<insert client id>',
-  clientSecret : '<insert client secret>',
+  clientId : 'a83fad3952264dbc9a14516d9fa9ccd9',
+  clientSecret : 'bb3b6f8c237f4e9ba43d12d232cd0da3',
 });
 
+var token;
 // Retrieve an access token
 spotifyApi.clientCredentialsGrant()
   .then(function(data) {
     console.log('The access token expires in ' + data.body['expires_in']);
     console.log('The access token is ' + data.body['access_token']);
 
+    token = data.body['access_token'];
+
     // Save the access token so that it's used in future calls
     spotifyApi.setAccessToken(data.body['access_token']);
   }, function(err) {
     console.log('Something went wrong when retrieving an access token', err.message);
   });
+  module.exports = token;
 
--- Music-recommendation-master1/Music-recommendation-master/Music-recommendation/tasks/getAlbums.js
+++ Music-recommendation-master1/Music-recommendation-master/Music-recommendation/tasks/getAlbums.js
@@ -6,17 +6,38 @@ const albums = require("../data/albums");
 const request = require('request');
 
 
-//const client = require("./client");
 
 var SpotifyWebApi = require('spotify-web-api-node');
 
-var spotifyApi = new SpotifyWebApi();
-spotifyApi.setAccessToken('BQBksA_hz1vx98JzomCPoDGWYBZ1jcEyNVthgYDTGL4ZIgkClM8HRZDJb1slTfPs2V-wGrwhEJ655RHHdlsoOEaB0wPC4zBk2PxOBRbx2Vu8dR9jbFGPL2jndYZotzT7N_qF4gsJLpsy6TzZgoHXVAwgyJT02KvdZQ');
+/**
+ * This example retrives an access token using the Client Credentials Flow. It's well documented here:
+ * https://developer.spotify.com/web-api/authorization-guide/#client_credentials_flow
+ */
 
+/*
+ * https://developer.spotify.com/spotify-web-api/using-scopes/
+ */
 
+ /**
+ * Set the credentials given on Spotify's My Applications page.
+ * https://developer.spotify.com/my-applications
+ */
+var spotifyApi = new SpotifyWebApi({
+  clientId : 'a83fad3952264dbc9a14516d9fa9ccd9',
+  clientSecret : 'bb3b6f8c237f4e9ba43d12d232cd0da3',
+});
 
+// Retrieve an access token
+spotifyApi.clientCredentialsGrant()
+  .then(function(data) {
+    console.log('The access token expires in ' + data.body['expires_in']);
+    console.log('The access token is ' + data.body['access_token']);
 
-
+    // Save the access token so that it's used in future calls
+    spotifyApi.setAccessToken(data.body['access_token']);
+  }, function(err) {
+    console.log('Something went wrong when retrieving an access token', err.message);
+  }).then(() => {
 
 
 spotifyApi.searchAlbums('Love')
@@ -53,3 +74,4 @@ spotifyApi.searchArtists('j')
   }).then(()=>{
             console.log("done");
         });
+})
