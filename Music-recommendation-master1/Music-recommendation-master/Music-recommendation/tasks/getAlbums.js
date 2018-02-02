1ef0a62bbddf518561da2df69da8adb0cbe90741
binglinxie <bxie3@stevens.edu>
2018-02-02 08:50:27 -0500

    change artist

--- Music-recommendation-master1/Music-recommendation-master/Music-recommendation/tasks/getAlbums.js
+++ Music-recommendation-master1/Music-recommendation-master/Music-recommendation/tasks/getAlbums.js
@@ -1,8 +1,8 @@
 const dbConnection = require("../config/mongoConnection");
 const user = require("../data/users");
 const tracks = require("../data/tracks");
-const artists = require("../data/artists"); 
-const albums = require("../data/albums"); 
+const artists = require("../data/artists");
+const albums = require("../data/albums");
 const request = require('request');
 
 
@@ -18,13 +18,13 @@ var SpotifyWebApi = require('spotify-web-api-node');
  * https://developer.spotify.com/spotify-web-api/using-scopes/
  */
 
- /**
+/**
  * Set the credentials given on Spotify's My Applications page.
  * https://developer.spotify.com/my-applications
  */
 var spotifyApi = new SpotifyWebApi({
-  clientId : 'a83fad3952264dbc9a14516d9fa9ccd9',
-  clientSecret : 'bb3b6f8c237f4e9ba43d12d232cd0da3',
+  clientId: 'a83fad3952264dbc9a14516d9fa9ccd9',
+  clientSecret: 'bb3b6f8c237f4e9ba43d12d232cd0da3',
 });
 
 // Retrieve an access token
@@ -37,41 +37,45 @@ spotifyApi.clientCredentialsGrant()
     spotifyApi.setAccessToken(data.body['access_token']);
   }, function(err) {
     console.log('Something went wrong when retrieving an access token', err.message);
-  }).then(() => {
+  })
+  .then(() => {
 
 
-spotifyApi.searchAlbums('Love')
-  .then(function(data) {
-    //console.log('Search artists by "Love"', data.body);
-    albums.addAllAlbum(data.body.albums.items);
-  }, function(err) {
-    console.error(err);
-  });  
+    spotifyApi.searchAlbums('beautiful')
+      .then(function(data) {
+        //console.log('Search artists by "Love"', data.body);
+        albums.addAllAlbum(data.body.albums.items);
+      }, function(err) {
+        console.error(err);
+      });
 
+    spotifyApi.searchAlbums('love')
+      .then(function(data) {
+        //console.log('Search artists by "Love"', data.body);
+        albums.addAllAlbum(data.body.albums.items);
+      }, function(err) {
+        console.error(err);
+      });
 
+    spotifyApi.searchArtists('a')
+      .then(function(data) {
+        //console.log('Search artists by "Love"', data.body);
+        artists.addAllArtist(data.body.artists.items)
+      }, function(err) {
+        console.error(err);
+      });
 
+    spotifyApi.searchTracks('love')
+      .then(function(data) {
+        //console.log('Search by "Love"', data.body.tracks.items);
 
-spotifyApi.searchArtists('j')
-  .then(function(data) {
-    //console.log('Search artists by "Love"', data.body);
-    artists.addAllArtist(data.body.artists.items)
-  }, function(err) {
-    console.error(err);
-  });
-    
+        //body = JSON.parse(data.body.tracks.items);
 
+        tracks.addAllTracks(data.body.tracks.items);
 
-  spotifyApi.searchTracks('x')
-  .then(function(data) {
-    //console.log('Search by "Love"', data.body.tracks.items);
-
-    //body = JSON.parse(data.body.tracks.items);
-
-    tracks.addAllTracks(data.body.tracks.items);
-
-  }, function(err) {
-    console.error(err);
-  }).then(()=>{
-            console.log("done");
-        });
-})
+      }, function(err) {
+        console.error(err);
+      }).then(() => {
+        console.log("done");
+      });
+  })
