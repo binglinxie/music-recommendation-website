a369e5ee91b31eba3dfef082f2c02250138df721
binglinxie <bxie3@stevens.edu>
2018-02-20 14:06:29 -0500

    edit readme

--- Music-recommendation-master1/Music-recommendation-master/Music-recommendation/tasks/seed.js
+++ Music-recommendation-master1/Music-recommendation-master/Music-recommendation/tasks/seed.js
@@ -1,157 +1,109 @@
-﻿const dbConnection = require("../config/mongoConnection");
+const dbConnection = require("../config/mongoConnection");
 const user = require("../data/users");
 const tracks = require("../data/tracks");
-const artists = require("../data/artists"); 
-const albums = require("../data/albums"); 
+const artists = require("../data/artists");
+const albums = require("../data/albums");
 const request = require('request');
-let i = 0;
 
-console.log("\n\n\n\n\n\n--- in seed.js prepare for db ---")
 
-dbConnection().then(db => {
-        return db.dropDatabase().then(() => {
-            return dbConnection;
-        })
-    })
-    .then((db) => {
-        let v1 = new Promise((resolve, reject) => {
-            request(`https://api.spotify.com/v1/search?q=em&type=artist&limit=50&offset=0`, function(error, response, body) {
-                if (!error && response.statusCode == 200) {                 
-                    body = JSON.parse(body);
-                    resolve(artists.addAllArtist(body.artists.items));
-                    
-                }
-            })
+
+var SpotifyWebApi = require('spotify-web-api-node');
+
+/**
+ * This example retrives an access token using the Client Credentials Flow. It's well documented here:
+ * https://developer.spotify.com/web-api/authorization-guide/#client_credentials_flow
+ */
+
+/*
+ * https://developer.spotify.com/spotify-web-api/using-scopes/
+ */
+
+/**
+ * Set the credentials given on Spotify's My Applications page.
+ * https://developer.spotify.com/my-applications
+ */
+var spotifyApi = new SpotifyWebApi({
+  clientId: 'a83fad3952264dbc9a14516d9fa9ccd9',
+  clientSecret: 'bb3b6f8c237f4e9ba43d12d232cd0da3',
+});
+
+// Retrieve an access token
+spotifyApi.clientCredentialsGrant()
+  .then(function(data) {
+    console.log('The access token expires in ' + data.body['expires_in']);
+    console.log('The access token is ' + data.body['access_token']);
+
+    // Save the access token so that it's used in future calls
+    spotifyApi.setAccessToken(data.body['access_token']);
+  }, function(err) {
+    console.log('Something went wrong when retrieving an access token', err.message);
+  })
+  .then(() => {
+ 
+      
+
+
+        let promise1 = spotifyApi.searchAlbums('beautiful')
+          .then(function(data) {
+            albums.addAllAlbum(data.body.albums.items);
+          }, function(err) {
+            console.error(err);
+          });
+
+        let promise2 = spotifyApi.searchAlbums('h')
+          .then(function(data) {
+            albums.addAllAlbum(data.body.albums.items);
+          }, function(err) {
+            console.error(err);
+          });
+
+        let promise3 = spotifyApi.searchAlbums('red')
+          .then(function(data) {
+            albums.addAllAlbum(data.body.albums.items);
+          }, function(err) {
+            console.error(err);
+          });
+
+        let promise4 = spotifyApi.searchAlbums('sea')
+          .then(function(data) {
+            albums.addAllAlbum(data.body.albums.items);
+          }, function(err) {
+            console.error(err);
+          });
+
+        let promise5 = spotifyApi.searchArtists('a')
+          .then(function(data) {
+            artists.addAllArtist(data.body.artists.items)
+          }, function(err) {
+            console.error(err);
+          });
+
+        let promise6 = spotifyApi.searchTracks('sea')
+          .then(function(data) {
+            tracks.addAllTracks(data.body.tracks.items);
+
+          }, function(err) {
+            console.error(err);
+          })
+        let promise7 = spotifyApi.searchTracks('pink')
+          .then(function(data) {
+            tracks.addAllTracks(data.body.tracks.items);
+
+          }, function(err) {
+            console.error(err);
+          })
+        let promise8 = spotifyApi.searchTracks('star')
+          .then(function(data) {
+            tracks.addAllTracks(data.body.tracks.items);
+
+          }, function(err) {
+            console.error(err);
+          })
+        Promise.all([promise1, promise2, promise3, promise4, promise5, promise6, promise7, promise8]).then(() => {
+          console.log("done");
         });
-        let v2 = new Promise((resolve, reject) => {
-            request(`https://api.spotify.com/v1/search?q=j&type=artist&limit=50&offset=0`, function(error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    body = JSON.parse(body);
-                    resolve(artists.addAllArtist(body.artists.items));
-                    
-                }
-            })
-        });
-        let v3 = new Promise((resolve, reject) => {
-            request(`https://api.spotify.com/v1/search?q=girl&type=album&limit=50&offset=0`, function(error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    body = JSON.parse(body);
-                    resolve(albums.addAllAlbum(body.albums.items));
-                    
-                }
-            })
-        });
-        let v4 = new Promise((resolve, reject) => {
-            request(`https://api.spotify.com/v1/search?q=it&type=album&limit=50&offset=0`, function(error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    body = JSON.parse(body);
-                    resolve(albums.addAllAlbum(body.albums.items));
-                    
-                }
-            })
-        });
-        let v5 = new Promise((resolve, reject) => {
-            request(`https://api.spotify.com/v1/search?q=s&type=album&limit=50&offset=0`, function(error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    body = JSON.parse(body);
-                    resolve(albums.addAllAlbum(body.albums.items));
-                    
-                }
-            })
-        });
-        let v6 = new Promise((resolve, reject) => {
-            request(`https://api.spotify.com/v1/search?q=love&type=album&limit=50&offset=0`, function(error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    body = JSON.parse(body);
-                    resolve(albums.addAllAlbum(body.albums.items));
-                    
-                }
-            })
-        });
-        let v7 = new Promise((resolve, reject) => {
-            request(`https://api.spotify.com/v1/search?q=r&type=album&limit=50&offset=0`, function(error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    body = JSON.parse(body);
-                    resolve(albums.addAllAlbum(body.albums.items));
-                    
-                }
-            })
-        });
-        let v8 = new Promise((resolve, reject) => {
-            request(`https://api.spotify.com/v1/search?q=l&type=album&limit=50&offset=0`, function(error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    body = JSON.parse(body);
-                    resolve(albums.addAllAlbum(body.albums.items));
-                    
-                }
-            })
-        });
-        let v9 = new Promise((resolve, reject) => {
-            request(`https://api.spotify.com/v1/search?q=j&type=track&limit=50&offset=0`, function(error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    body = JSON.parse(body);
-                    resolve(tracks.addAllTracks(body.tracks.items));
-                    
-                }
-            });
-
-        });
-        let v10 = new Promise((resolve, reject) => {
-            request('https://api.spotify.com/v1/search?q=s&type=track&limit=50&offset=0', function(error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    body = JSON.parse(body);
-
-                    resolve(tracks.addAllTracks(body.tracks.items));
-                    
-                }
-            });
-
-        });
-        return Promise.all([v1, v2, v3, v4, v5, v6, v7, v8, v9, v10]);
-
-    }).then((res) => {
-  
-        albums.getAllAlbums().then((Items) => {
-            let a = [];
-            for (let i = 0; i < Items.length; i++) {
-                a[i] = new Promise((resolve, reject) => {
-                    request(`https://api.spotify.com/v1/albums/${Items[i].id}`, function(error, response, body) {
-                        if (!error && response.statusCode == 200) {
-                            body = JSON.parse(body);
-                            resolve(albums.addTracksToAlbum(Items[i].id, body.tracks));
-                
-                        }
-                    })
-
-                })
-                
-
-
-            }
-            return Promise.all(a);
-
-        }).then(()=>{
-            console.log("done");
-        })
-    })
-
-
-console.log("First user created")
-   user.addUser("aaa", "123").then((newUser) => {
-        id = newUser._id;
-        console.log(newUser);
-    }).catch(err => {
-        console.log("error occurs" + err);
-    });
-
-
-
-
-
-
-
-
-
-
-
-
+      
+    // .then(() => {
+    //   console.log("done");
+    // });
+  })
